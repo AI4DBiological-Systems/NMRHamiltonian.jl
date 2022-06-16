499fda491473a7d474d1200c0119c5ae39903b1c
Roy <rwang013@uottawa.ca>
2022-06-16 00:22:55 -0400

    moved some methods and types to NMRSignalModels.jl

--- src/types.jl
+++ src/types.jl
@@ -19,35 +19,3 @@ struct SHType{T} # output of the SH simulation.
     SW::T
     ν_0ppm::T
 end
-
-
-# different parameterizations of the spin system FID parameters.
-
-
-struct SpinSysParamsType2{T}
-    κs_λ::Vector{Vector{T}} # a multiplier for each (spin group, partition element).
-    κs_β::Vector{Vector{T}} # a vector coefficient for each (spin group). vector length: number of spins in the spin group.
-    d::Vector{Vector{T}} # a multiplier for each (spin group, partition element).
-end
-
-function SpinSysParamsType2(x::T) where T
-    return SpinSysParamsType2(Vector{Vector{T}}(undef,0), Vector{Vector{T}}(undef, 0), Vector{Vector{T}}(undef, 0))
-end
-
-struct SpinSysParamsType1{T}
-    κs_λ::Vector{T} # a common multiplier for each spin group. length: number of spin groups.
-    κs_β::Vector{Vector{T}} # a vector coefficient for each (spin group). vector length: number of spins in the spin group.
-    d::Vector{T} # a common multiplier for each spin group. length: number of spin groups.
-end
-
-function SpinSysParamsType1(x::T) where T
-    return SpinSysParamsType1(Vector{T}(undef,0), Vector{Vector{T}}(undef, 0), Vector{T}(undef, 0))
-end
-
-function constructorSSFID(x::SpinSysParamsType1{T}, y...)::SpinSysParamsType1{T} where T
-    return SpinSysParamsType1(y...)
-end
-
-# function constructorSSFID(x::SpinSysFIDType2{T}, y...)::SpinSysFIDType2{T} where T
-#     return SpinSysFIDType2(y...)
-# end
